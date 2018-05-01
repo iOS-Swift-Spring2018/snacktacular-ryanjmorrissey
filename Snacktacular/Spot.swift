9f42e1f19fc0b6bdeb15ceecb0e1b44922a6264f
Ryan Morrissey <ryanmorrissey123@me.com>
2018-05-01 17:41:30 -0400

    Snacktacular app

---
+++ Snacktacular/Spot.swift
@@ -0,0 +1,31 @@
+//
+//  Spot.swift
+//  Snacktacular
+//
+//  Created by Ryan Morrissey on 5/1/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class Spot {
+    var name: String
+    var address: String
+    var coordinate: CLLocationCoordinate2D
+    var averageRating: Double
+    var numberOfReviews: Int
+    var postingUserID: String
+    var documentID: String
+    
+    init(name: String, address: String, coordinate: CLLocationCoordinate2D, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
+        self.name = name
+        self.address = address
+        self.coordinate = coordinate
+        self.averageRating = averageRating
+        self.numberOfReviews = numberOfReviews
+        self.postingUserID = postingUserID
+        self.documentID = documentID
+        
+    }
+}
---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,9 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Ryan Morrissey on 5/1/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import Foundation
